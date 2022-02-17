b89e4bba6d6a99aa14f6587f5c896e6a46bbbd47
lukifr <lukifraniewski@gmail.com>
2022-02-17 23:04:36 +0100

    commit 2

--- manage-company/src/components/MainScreen/MainScreen.js
+++ manage-company/src/components/MainScreen/MainScreen.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Paper from "@mui/material/Paper";
 import Month from "./Month/Month.js";
 import WorkDay from "../WorkDay/WorkDay.js";
+import { monthsToQuarters } from "date-fns";
 
 const StyledMainScreen = styled.div`
   display: flex;
@@ -15,6 +16,7 @@ const MainScreen = () => {
   const [workDays, setWorkDays] = useState([]);
   const [months, setMonths] = useState([]);
   const [totalWorkTime, setTotalWorkTime] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const findMonths = () => {
     setMonths(
@@ -40,8 +42,61 @@ const MainScreen = () => {
   };
 
   const calcTotalWorkTime = () => {
-    const x = months.map((x) => [x]);
-    console.log(x);
+    const x = months
+      .map((x) => {
+        return { month: x, users: [] };
+      })
+      .filter(
+        (v, i, a) =>
+          a.findIndex((t) => ["month"].every((k) => t[k] === v[k])) === i
+      );
+
+    x.forEach((x) => {
+      workDays.forEach((y) => {
+        if (x.month === y.date.split("/")[1] + "/" + y.date.split("/")[2]) {
+          x.users = [...x.users, ...y.users];
+        }
+      });
+    });
+
+    const result = x.map((x) => {
+      const temp = x.users.reduce(function (results, org) {
+        (results[org._id] = results[org._id] || []).push(org);
+        return results;
+      }, []);
+
+      const final = Object.values(temp).map((x) => {
+        const totalTime = () => {
+          const time = x.map((y) => y.totalTime);
+          let hours = 0,
+            minutes = 0;
+          time.forEach((time) => {
+            const split = time.split(":");
+            hours += parseInt(split[0]);
+            minutes += parseInt(split[1]);
+          });
+          const formattedNumber = (num) => ("0" + num).slice(-2);
+          hours += Math.floor(minutes / 60);
+          minutes = minutes % 60;
+
+          return formattedNumber(hours) + ":" + formattedNumber(minutes);
+        };
+
+        return {
+          _id: x[0]._id,
+          firstName: x[0].firstName,
+          lastName: x[0].lastName,
+          totalTime: totalTime(),
+        };
+      });
+
+      return {
+        month: x.month,
+        users: final,
+      };
+    });
+
+    setTotalWorkTime(result);
   };
 
   const fetchServerData = () => {
@@ -52,19 +107,26 @@ const MainScreen = () => {
 
   useEffect(() => {
     fetchServerData();
+
+    axios
+      .get("http://localhost:8080/users")
+      .then((res) => res.data)
+      .then((res) => setUsers(res));
   }, []);
 
   useEffect(() => {
     findMonths();
-    console.log(workDays);
   }, [workDays]);
+
+  useEffect(() => {
+    calcTotalWorkTime();
+  }, [months]);
 
   return (
     <StyledMainScreen>
-      <button onClick={calcTotalWorkTime}>CLICK</button>
       <Paper sx={{ width: 1000, maxWidth: "100%" }}>
-        {months.map((x) => (
-          <Month month={x} />
+        {totalWorkTime.map((x, index) => (
+          <Month key={index} month={x.month} users={x.users} />
         ))}
       </Paper>
     </StyledMainScreen>
--- manage-company/src/components/MainScreen/Month/Month.js
+++ manage-company/src/components/MainScreen/Month/Month.js
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "@mui/material/Paper";
 import styled from "styled-components";
+import { DataGrid } from "@mui/x-data-grid";
 
 const StyledMonth = styled.p`
-  position: absolute;
-  margin: 20px 20px;
+  position: relative;
+  margin: 0px 20px;
   font-size: 20px;
   font-weight: bold;
 `;
 
-const Month = ({ month }) => {
+const columns = [
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 140,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 140,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "totalTime",
+    headerName: "Total Time",
+    width: 140,
+    headerAlign: "center",
+    align: "center",
+  },
+];
+
+const Month = ({ month, users }) => {
+  const [tabbleUsers, setTabbleUsers] = useState([]);
+
   return (
     <Paper
-      sx={{ width: "85%", maxWidth: "100%", height: 100, margin: "20px auto" }}
+      sx={{
+        width: "85%",
+        maxWidth: "100%",
+        margin: "20px auto",
+        padding: "20px 0px",
+      }}
     >
       <StyledMonth>{month}</StyledMonth>
+      <DataGrid
+        sx={{ margin: "30px" }}
+        getRowId={(row) => row._id}
+        rows={users}
+        columns={columns}
+        pageSize={50}
+        rowsPerPageOptions={[50]}
+        autoHeight
+        disableColumnFilter={true}
+        disableColumnMenu
+      />
     </Paper>
   );
 };
